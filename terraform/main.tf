1f70973e12e448d12459f12f0e69fe62fe421b14
Kapil <kapilsreed@gmail.com>
2020-06-21 14:24:44 +0200

    Terraform GCP config

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("blade-ai-bd37d3c584bb.json")
+
+  project = "blade-ai-280814"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+  
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+ 
