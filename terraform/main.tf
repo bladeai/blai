cd461e07ad9e79c2bcf95b71ae74fad9b900aa12
bladeaico <bladeaico@gmail.com>
2020-06-22 23:02:43 +0200

    Update terrafrom

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,25 +8,54 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 8
 }
-  
+
+
+
+resource "google_compute_firewall" "default" {
+  name    = "terraform-firewall"
+  network = "default"
+ 
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000", "22"]
+  }
+
+  source_tags = ["ssh"]
+}  
+
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "tf-blade-${random_id.instance_id.hex}"
   machine_type = "f1-micro"
-  tags = ["web", "dev"]
 
   boot_disk {
     initialize_params {
-      image = "cos-cloud/cos-stable"
+     image = "debian-cloud/debian-9"
     }
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
+   network = "default"
     access_config {
     }
   }
-}
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install; cd ~;git clone https://github.com/hashicorp/learn-terraform-provision-gke-cluster; cd learn-terraform-provision-gke-cluster"
  
+ metadata = {
+   ssh-keys = "bladeaico:${file("/Users/kapilsreedharan/.ssh/id_rsa_bladeaico.pub")}"
+ }
+
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
+// A variable for extracting the external IP address of the instance
+output "ip" {
+ value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+}
