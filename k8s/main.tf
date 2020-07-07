bbb098a180f5ca930dc4ebd9522eb1e674edd207
ksree <kapilsreed@gmail.com>
2020-07-07 23:04:42 +0200

    add gke creator

---
+++ k8s/main.tf
@@ -0,0 +1,17 @@
+
+# GKE cluster
+provider "google" {
+  project  = "blade-ai-282114"
+  region   = "europe-west3"
+}
+
+resource "google_container_cluster" "gke-cluster" {
+  name               = "bladeai-gke-cluster"
+  network            = "default"
+  location = "europe-west3-a"
+  initial_node_count = 3
+  node_config {
+    machine_type = "g1-small"
+  }
+}
+
